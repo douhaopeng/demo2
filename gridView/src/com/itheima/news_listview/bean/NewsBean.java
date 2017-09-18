ad53a24e72752cbf4be3135bb43fe8e2e46d5583
douhaopeng <1591994251@qq.com>
2017-09-18 10:19:46 +0800

    gridviewdemo

---
+++ gridView/src/com/itheima/news_listview/bean/NewsBean.java
@@ -0,0 +1,13 @@
+package com.itheima.news_listview.bean;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+public class NewsBean {
+
+	
+	public String des;
+	public Drawable icon; 
+	
+	
+}
