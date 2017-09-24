87ecee5849a24855dbd3dbccaca37c633cf4f411
douhaopeng <1591994251@qq.com>
2017-09-24 14:33:35 +0800

    mSlidingMunu

---
+++ mSlidingMenu/app/src/main/java/com/example/douhaopeng/zhbjas1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.douhaopeng.zhbjas1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+/**
+ * Created by Administrator on 2017\9\24 0024.
+ */
+
+public  class MainActivity extends SlidingFragmentActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        setBehindContentView(R.layout.left_menu);
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setBehindOffset(500);
+
+    }
+}
